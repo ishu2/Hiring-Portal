60a95119d8048bd7e6d3131d7e72e7fc7240ce57
ishu2 <rathiishita123@gmail.com>
2018-02-15 21:46:54 +0530

    DATABASE Ready

--- models/models.js
+++ models/models.js
@@ -14,8 +14,94 @@ var Student=db.define('student',{
     projects: Sequelize.ARRAY(Sequelize.JSON),
 });
 
-const CompanyManager = db.define('companymanager', {
+var CompanyManager = db.define('companymanager', {
     id: {type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true},
     designation: Sequelize.STRING,
 });
 
+var Admin=db.define('admin',{
+    id:{type:Sequelize.INTEGER, primaryKey:true, autoIncrement:true},
+    designation:Sequelize.STRING
+});
+
+var User=db.define('user',{
+    id:{type:Sequelize.INTEGER, primaryKey:true, autoIncrement:true},
+    name:Sequelize.STRING,
+    contact:Sequelize.STRING,
+    email:Sequelize.STRING,
+    image:Sequelize.STRING
+});
+
+Student.belongsTo(User);
+User.hasOne(Student);
+CompanyManager.belongsTo(User);
+User.hasOne(CompanyManager);
+Admin.belongsTo(User);
+User.hasOne(Admin);
+
+var OneAuth=db.define('authtoken',{
+    id:{type:Sequelize.INTEGER, primaryKey:true, autoIncrement:true},
+    oneauthId:Sequelize.INTEGER,
+    oneauthToken:Sequelize.STRING,
+    token:Sequelize.STRING
+});
+
+OneAuth.belongsTo(User);
+User.hasMany(OneAuth);
+
+var Company=db.define('company',{
+    id:{type:Sequelize.INTEGER, primaryKey:true, autoIncrement:true},
+    name:Sequelize.STRING,
+    website:Sequelize.STRING,
+    locations:Sequelize.ARRAY(Sequelize.STRING),
+    skills:Sequelize.ARRAY(Sequelize.STRING),
+    contactEmail:Sequelize.STRING,
+    contactNumber:Sequelize.STRING
+});
+
+var Job=db.define('job',{
+    id:{type:Sequelize.INTEGER, autoIncrement:true, primaryKey:true},
+    title:Sequelize.String,
+    description:Sequelize.STRING,
+    skills:Sequelize.ARRAY(Sequelize.STRING),
+    jobType:Sequelize.STRING,
+    location:Sequelize.STRING,
+    salary:Sequelize.STRING,
+    active:Sequelize.BOOLEAN,
+    startDate:Sequelize.STRING,
+    endDate:Sequelize.STRING
+});
+
+var Application=db.define('application'{
+    id:{type:Sequelize.INTEGER, primaryKey:true, autoIncrement:true},
+    status:Sequelize.STRING,
+    date:Sequelize.INTEGER,
+    application:Sequelize.STRING
+});
+
+CompanyManager.belongsTo(Company);
+Company.hasMany(CompanyManager);
+
+Job.belongsTo(Company);
+Company.hasMany(CompanyManager);
+
+Job.belongsTo(Company);
+Company.hasMany(Job);
+
+Application.belongsTo(User);
+User.hasMany(Application);
+
+Application.belongsTo(Job);
+Job.hasMany(Application);
+
+module.exports={
+    Student,
+    CompanyManager,
+    Admin,
+    User,
+    Company,
+    Job,
+    Application,
+    OneAuth,
+    db
+};
