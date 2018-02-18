dfe9ef21455fa2b62c7e711653b63189a753f398
ishu2 <rathiishita123@gmail.com>
2018-02-18 21:11:08 +0530

    Check for managers

--- auth/authutils.js
+++ auth/authutils.js
@@ -1,3 +1,29 @@
 var models=require('./../models/models').models;
 var config=require('./../config');
 
+function ensureLogin(){
+    return function(req,res,next){
+        if(config.DEV_MODE || req.user){
+            next();
+        }else{
+            res.status(401).send("Please login first..");
+        }
+    }
+}
+
+function ensureCompanyManager(){
+    return function(req,res,next){
+        if(req.user){
+            models.CompanyManager.findOne({
+                where:{userId:req.user.id}
+            }).then(function(user){
+                if(user)
+                  next();
+                else 
+                  res.status(401).send("Only company managers are allowed..");
+            })
+        } else{
+            res.status(401).send("Please login first..");
+        }
+    }
+}
