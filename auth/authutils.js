f0098d5d494da9be47a4d437be69c51ebe315e0b
ishu2 <rathiishita123@gmail.com>
2018-02-20 19:48:13 +0530

    authorizarion completed

--- auth/authutils.js
+++ auth/authutils.js
@@ -27,3 +27,38 @@ function ensureCompanyManager(){
         }
     }
 }
+
+function ensureAdmin(){
+    return function(req,res,next){
+        if(req.user){
+            models.Admin.findOne({
+                where:{userId:req.user.id}
+            }).then(function(user){
+                if(user)
+                  next();
+                else
+                  res.status(401).send("Only admins are allowed..");
+            })
+        } else{
+            res.status(401).send("Please login first..");
+        }
+    }
+}
+
+
+function ensureStudent(){
+    return function(req,res,next){
+        if(req.user){
+            models.Student.findOne({
+                where:{userId:req.user.id}
+            }).then(function(user){
+                if(user)
+                  next();
+                else
+                  res.status(401).send("Only students are allowed..");
+            })
+        } else{
+            res.status(401).send("Please login first..");
+        }
+    }
+}
