eb9fd861509e25f21dac7da17bc845ea321f2217
ishu2 <rathiishita123@gmail.com>
2018-02-24 23:12:12 +0530

    admin added

--- routes/api/admin.js
+++ routes/api/admin.js
@@ -3,3 +3,23 @@ var models=require('./../../models/models').models;
 var password=require('./../../utils/password');
 var ensure=require('./../../auth/authutils');
 var passport=require('./../../auth/passporthandler');
+
+router.post('/add',passport.authenticate('bearer'),ensure.ensureAdmin,function(req,res){
+    if(!req.body.userId===true){
+        res.status(400).send("Only valid users can be made admins..");
+    }
+
+    models.Admin.create({
+        centre:req.body.centre,
+        designation:req.body.designation,
+        userId:req.body.userId
+    }).then(function(admin){
+        if(admin)
+          res.status(201).send("Admin created..");
+        else
+          res.status(500).send("Could not create the Admin..");
+    }).catch(function(err){
+        console.log(err);
+        res.status(500).send("Could not create the Admin..");
+    });
+});
