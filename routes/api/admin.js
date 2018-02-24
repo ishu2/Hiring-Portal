4a044baf175e3e1d488052e6c5abf9c86d05556c
ishu2 <rathiishita123@gmail.com>
2018-02-24 23:00:27 +0530

    admin

---
+++ routes/api/admin.js
@@ -0,0 +1,5 @@
+var router=require('express').Router();
+var models=require('./../../models/models').models;
+var password=require('./../../utils/password');
+var ensure=require('./../../auth/authutils');
+var passport=require('./../../auth/passporthandler');
