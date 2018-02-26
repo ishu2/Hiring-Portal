28aab0524c763c1f719221d69e7058bffc70010c
ishu2 <rathiishita123@gmail.com>
2018-02-26 14:44:46 +0530

    Post

--- routes/api/companies.js
+++ routes/api/companies.js
@@ -4,3 +4,10 @@ var password = require('./../../utils/password');
 var ensure = require('./../../auth/authutils');
 var passport = require('./../../auth/passporthandler');
 var config = require('./../../config');
+
+router.post('/add',passport.authenticate('bearer'),ensure.ensureAdmin(),function(req,res){
+  if(req.body.name===""){
+      res.status(403).send("Insufficient Details..");
+  }    
+  
+})
