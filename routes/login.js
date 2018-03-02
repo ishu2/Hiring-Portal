edf46e725078b987a113bcc06b11f314bf9e8d51
ishu2 <rathiishita123@gmail.com>
2018-03-02 23:54:46 +0530

    Login routes

--- routes/login.js
+++ routes/login.js
@@ -1,6 +1,35 @@
 var route=require('express').Router();
+var passport=require('./../auth/passporthandler');
 
-route.get('/login',function(req,res){
-    res.render('login');
+route.get('/',function(req,res){
+    res.render('login-landing-page',{});
 });
 
+route.get('/student', (req, res) => {
+    res.render("login", {role: 'student'})
+});
+
+route.get('/company', (req, res) => {
+    res.render("login", {role: 'company'})
+});
+
+route.get('/admin', (req, res) => {
+    res.render("login", {role: 'admin'})
+});
+
+route.post('/student', passport.authenticate('local-student', {
+    failureRedirect: '/login/student',
+    successRedirect: '/profile'
+}));
+
+route.post('/company', passport.authenticate('local-company', {
+    failureRedirect: '/login/company',
+    successRedirect: '/profile'
+}));
+
+route.post('/admin', passport.authenticate('local-admin', {
+    failureRedirect: '/login/admin',
+    successRedirect: '/profile'
+}));
+
+module.exports = route;
