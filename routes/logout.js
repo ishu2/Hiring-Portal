46a3d7de9d087e195fa942d8f4e21c2617885f8c
ishu2 <rathiishita123@gmail.com>
2018-03-03 17:06:11 +0530

    Logout

---
+++ routes/logout.js
@@ -0,0 +1,11 @@
+var route = require('express').Router();
+
+route.get('/', (req, res) => {
+    req.user = null;
+    req.logout();
+    req.session.destroy(() => {
+        res.redirect('/login')
+    });
+});
+
+module.exports = route;
