616685713c9959b366682236330192cc5247baf2
ishu2 <rathiishita123@gmail.com>
2018-03-04 15:27:04 +0530

    Create student

--- routes/signup.js
+++ routes/signup.js
@@ -18,3 +18,38 @@ route.get('/company', (req, res) => {
 route.get('/admin', (req, res) => {
     res.render("signup", {role: 'admin', isAdmin: true});
 });
+
+route.post('/student', (req, res) => {
+    if (req.body.name === "" || req.body.email === "" || req.body.password === "") {
+        res.status(403).send("Insufficient Details");
+    }
+    password.pass2hash(req.body.password).then(function (hash) {
+        console.log(password);
+        console.log(hash);
+        models.UserLocal.create({
+            email: req.body.email,
+            password: hash,
+            role: "Student",
+            companyId: 0,
+            adminId: 0,
+            student: {
+                name: req.body.name,
+                email: req.body.email
+            }
+        }, {
+            include: [models.Student]
+        }).then(function (studentLocal) {
+            if (studentLocal) {
+                res.status(200).send({success: true});
+            } else {
+                res.status(404).send({success: false});
+            }
+        }).catch(function (err) {
+            console.log(err);
+            res.status(500).send({success: 'error'});
+        })
+    }).catch(function (err) {
+        console.log(err);
+        res.status(500).send({success: 'error'});
+    });
+});
